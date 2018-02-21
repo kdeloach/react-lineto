24a4c782e42ae8fbef105b33d420bc2b35805a03
Kevin DeLoach <kdeloach@gmail.com>
2018-02-21 17:04:13 -0500

    Add SteppedLine and SteppedLineTo components
    This kind of line may be useful for building tree structures.
    
    Adapted from https://github.com/kdeloach/react-lineto/pull/2
    
    Closes #3

--- src/index.jsx
+++ src/index.jsx
@@ -168,6 +168,15 @@ LineTo.propTypes = Object.assign({}, {
     delay: PropTypes.oneOfType([PropTypes.number, PropTypes.bool]),
 }, optionalStyleProps);
 
+export class SteppedLineTo extends LineTo {
+    render() {
+        const points = this.detect();
+        return points ? (
+            <SteppedLine {...points} {...this.props} />
+        ) : null;
+    }
+}
+
 export class Line extends PureComponent {
     componentDidMount() {
         // Append rendered DOM element to the container the
@@ -235,4 +244,69 @@ Line.propTypes = Object.assign({}, {
     y0: PropTypes.number.isRequired,
     x1: PropTypes.number.isRequired,
     y1: PropTypes.number.isRequired,
+}, optionalStyleProps);
+
+export class SteppedLine extends PureComponent {
+    render() {
+        if (this.props.orientation === 'h') {
+            return this.renderHorizontal();
+        }
+        return this.renderVertical();
+    }
+
+    renderVertical() {
+        const { x0, y0, x1, y1 } = this.props;
+
+        const dx = x1 - x0;
+        if (dx === 0) {
+            return <Line {...this.props} />
+        }
+
+        const borderWidth = this.props.borderWidth || defaultBorderWidth;
+        const y2 = (y0 + y1) / 2;
+
+        const xOffset = dx > 0 ? borderWidth : 0;
+        const minX = Math.min(x0, x1) - xOffset;
+        const maxX = Math.max(x0, x1);
+
+        return (
+            <div className="react-steppedlineto">
+                <Line {...this.props} x0={x0} y0={y0} x1={x0} y1={y2} />
+                <Line {...this.props} x0={x1} y0={y1} x1={x1} y1={y2} />
+                <Line {...this.props} x0={minX} y0={y2} x1={maxX} y1={y2} />
+            </div>
+        );
+    }
+
+    renderHorizontal() {
+        const { x0, y0, x1, y1 } = this.props;
+
+        const dy = y1 - y0;
+        if (dy === 0) {
+            return <Line {...this.props} />
+        }
+
+        const borderWidth = this.props.borderWidth || defaultBorderWidth;
+        const x2 = (x0 + x1) / 2;
+
+        const yOffset = dy < 0 ? borderWidth : 0;
+        const minY = Math.min(y0, y1) - yOffset;
+        const maxY = Math.max(y0, y1);
+
+        return (
+            <div className="react-steppedlineto">
+                <Line {...this.props} x0={x0} y0={y0} x1={x2} y1={y0} />
+                <Line {...this.props} x0={x1} y0={y1} x1={x2} y1={y1} />
+                <Line {...this.props} x0={x2} y0={minY} x1={x2} y1={maxY} />
+            </div>
+        );
+    }
+}
+
+SteppedLine.propTypes = Object.assign({}, {
+    x0: PropTypes.number.isRequired,
+    y0: PropTypes.number.isRequired,
+    x1: PropTypes.number.isRequired,
+    y1: PropTypes.number.isRequired,
+    orientation: PropTypes.oneOf(['h', 'v']),
 }, optionalStyleProps);
