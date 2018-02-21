b2da834c02538280562ef5c45424842570e15baf
Kevin DeLoach <kdeloach@gmail.com>
2018-02-21 17:06:06 -0500

    Add SteppedLineTo demo

--- demo/index.jsx
+++ demo/index.jsx
@@ -3,12 +3,13 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { render } from 'react-dom';
 
-import LineTo, { Line } from '../src/index.jsx';
+import LineTo, { SteppedLineTo, Line } from '../src/index.jsx';
 
 function Demo() {
     return (
         <div>
             <PolygonTest />
+            <SteppedTest />
             <HoverTest />
             <DelayTest />
         </div>
@@ -76,6 +77,71 @@ class PolygonTest extends Component {
                 {triangle}
                 {star}
                 {ngon}
+            </fieldset>
+        );
+    }
+}
+
+class SteppedTest extends Component {
+    render() {
+        const style = {
+            delay: true,
+            borderColor: '#ddd',
+            borderStyle: 'solid',
+            borderWidth: 3,
+        };
+        return (
+            <fieldset id="stepped-test">
+                <legend>Stepped Test</legend>
+
+                Demonstrate how to draw stepped lines.
+
+                <Block
+                    className="stepped-A"
+                    top="50px"
+                    left="90px"
+                    color="#00f"
+                    >A</Block>
+                <Block
+                    className="stepped-B"
+                    top="150px"
+                    left="20px"
+                    color="#00f"
+                    >B</Block>
+                <Block
+                    className="stepped-C"
+                    top="150px"
+                    left="90px"
+                    color="#00f"
+                    >C</Block>
+                <Block
+                    className="stepped-D"
+                    top="150px"
+                    left="160px"
+                    color="#00f"
+                    >D</Block>
+                <Block
+                    className="stepped-E"
+                    top="50px"
+                    left="300px"
+                    color="#00f"
+                    >E</Block>
+                <Block
+                    className="stepped-F"
+                    top="120px"
+                    left="300px"
+                    color="#00f"
+                    >F</Block>
+                <SteppedLineTo from="stepped-A" to="stepped-B"
+                    fromAnchor="bottom" toAnchor="top" {...style} />
+                <SteppedLineTo from="stepped-A" to="stepped-C"
+                    fromAnchor="bottom" toAnchor="top" {...style} />
+                <SteppedLineTo from="stepped-A" to="stepped-D"
+                    fromAnchor="bottom" toAnchor="top" {...style} />
+                <SteppedLineTo from="stepped-A" to="stepped-E"
+                    fromAnchor="right" toAnchor="left" orientation="h" {...style} />
+                <SteppedLineTo from="stepped-A" to="stepped-F"
+                    fromAnchor="right" toAnchor="left" orientation="h" {...style} />
             </fieldset>
         );
     }
